0feac722c96540e8dcb7d1d16d3643d4d7c57c55
robertmoore40 <robertcmoore92@gmail.com>
2021-02-18 13:26:51 -0500

    added constructor

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,3 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Migrations {
     address public owner;
   uint public last_completed_migration;
+
+    constructor() public {
+    owner = msg.sender;
+  }
