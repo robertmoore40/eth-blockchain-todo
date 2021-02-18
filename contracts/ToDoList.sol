901284c8850ca04e4c8d3a61a005f89f384727b6
robertmoore40 <robertcmoore92@gmail.com>
2021-02-18 13:33:05 -0500

    added emit

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -37,3 +37,9 @@ contract TodoList {
    function toggleCompleted(uint _id) public {
     Task memory _task = tasks[_id];
     _task.completed = !_task.completed;
+
+     tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+}
