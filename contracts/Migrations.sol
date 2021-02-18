40bde3dec68876ebca8275923068092f51df1a21
robertmoore40 <robertcmoore92@gmail.com>
2021-02-18 13:28:35 -0500

    added upgrade

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -18,3 +18,6 @@ modifier restricted() {
 
    function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
+     upgraded.setCompleted(last_completed_migration);
+  }
+}
