cd2b5cb120da38b039bc7a0fc293abf3efc0f1ae
robertmoore40 <robertcmoore92@gmail.com>
2021-02-19 10:35:55 -0500

    added event

--- test/ToDoList.test.js
+++ test/ToDoList.test.js
@@ -36,3 +36,5 @@ it('toggles task completion', async () => {
     const result = await this.todoList.toggleCompleted(1)
     const task = await this.todoList.tasks(1)
     assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
