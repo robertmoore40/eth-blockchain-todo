a7fa8823a8959f6075a6e990dbd8fcb337a2b137
robertmoore40 <robertcmoore92@gmail.com>
2021-02-19 10:31:34 -0500

    added deploy message

--- test/ToDoList.test.js
+++ test/ToDoList.test.js
@@ -4,3 +4,6 @@ contract('TodoList', (accounts) => {
     before(async () => {
         this.todoList = await TodoList.deployed()
   })
+
+  it('deploys successfully', async () => {
+    const address = await this.todoList.address
