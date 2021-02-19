365212925122d13639571c1a5efa912def9ea878
robertmoore40 <robertcmoore92@gmail.com>
2021-02-19 10:31:47 -0500

    added addresses

--- test/ToDoList.test.js
+++ test/ToDoList.test.js
@@ -7,3 +7,5 @@ contract('TodoList', (accounts) => {
 
   it('deploys successfully', async () => {
     const address = await this.todoList.address
+    assert.notEqual(address, 0x0)
+    assert.notEqual(address, '')
